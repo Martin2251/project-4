38d8f089fe97d451cb8c65d9e2e5d5b344d2e366
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2022-05-07 11:26:09 +0100

    start of mongo

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+
+
+
+// mongodb+srv://martincode:<password>@cluster0.bha2m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.json({limite: "30mb", extended:true}));
 
 const port = 5000;
-
-const app = express();
 
 app.get("/", (req,res) => {
     res.send("hELLO EXPRESS");
